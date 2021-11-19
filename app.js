ce9bd3d19a0d28ef9cd116f7ce5c3397fe02e5a0
serhat <serhatsilsupur@gmail.com>
2021-11-19 10:54:08 +0300

    2.commit

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log("Function working");
-};
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    const photo= {
+        id:1,
+        name:"photo name",
+        description:"photo describtion"
+    }
+  res.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
